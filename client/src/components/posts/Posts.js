0187a81917f5b2369deb63c41dbcaacf1db10e92
Vibhu <vibhu98mishra@gmail.com>
2019-01-18 13:35:08 +0530

    post creation module added

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import PostForm from './PostForm'
+import Spinner from '../common/Spinner'
+
+class Posts extends Component {
+  render() {
+    return (
+          <PostForm />
+    )
+  }
+}
+
+export default Posts;
