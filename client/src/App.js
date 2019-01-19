8393d35004f278e1051ea02d40f50d64dbe652e3
Vibhu <vibhu98mishra@gmail.com>
2019-01-19 18:47:50 +0530

    Add comment to post

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/posts/Post';
 import NotFound from './components/not-found/NotFound';
 
 //Check for token
@@ -97,6 +98,13 @@ class App extends Component {
                   exact 
                   path="/feed" 
                   component={Posts} 
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute 
+                  exact 
+                  path="/post/:id" 
+                  component={Post} 
                 />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,6 +5,7 @@ import {
     GET_ERRORS,
     POST_LOADING,
     GET_POSTS,
+    GET_POST,
     DELETE_POST
 } from './types';
 
@@ -43,6 +44,24 @@ export const getPosts = () => dispatch => {
         )
 }
 
+// GET Post
+export const getPosts = (id) => dispatch => {
+    dispatch(setPostLoading());
+    axios.get(`/api/posts/${id}`)
+        .then(res => 
+            dispatch({
+                type: GET_POST,
+                payload: res.data
+            })    
+        )    
+        .catch(err =>
+            dispatch({
+                type: GET_POST,
+                payload: null
+            })    
+        )
+}
+
 // Delete Post 
 export const deletePost = id => dispatch => {
     axios.delete(`/api/posts/${id}`)
@@ -76,6 +95,23 @@ export const addLike = id => dispatch => {
 export const removeLike = id => dispatch => {
     axios.post(`/api/posts/unlike/${id}`)
         .then(res => dispatch(getPosts()))    
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })    
+        )
+}
+
+// Add Comment 
+export const addComment = (postId, commentData) => dispatch => {
+    axios.post(`/api/posts/comment/${postId}`, commentData)
+        .then(res => 
+            dispatch({
+                type: GET_POST,
+                payload: res.data
+            })    
+        )    
         .catch(err =>
             dispatch({
                 type: GET_ERRORS,
