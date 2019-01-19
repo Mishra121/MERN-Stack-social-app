c1a330dbad9e6ca06785a861d47f9f4cfced8733
Vibhu <vibhu98mishra@gmail.com>
2019-01-19 19:13:04 +0530

    comment display and delete, and clear errors modules

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import Spinner from '../common/Spinner'
 import PostItem from '../posts/PostItem'
 import CommentForm  from './CommentForm'
+import CommentFeed from './CommentFeed'
 
 import { getPost } from '../../actions/postActions'
 
@@ -27,6 +28,7 @@ class Post extends Component {
                 <div>
                     <PostItem post={post} showActions={false} />
                     <CommentForm postId={post._id} />
+                    <CommentFeed postId={post._id} comments={post.comments} />
                 </div>
             );
         }
